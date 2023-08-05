f4256d90340d54c2dd6f46efc8bd128f4c424d2e
tukumo-n9 <tukumon9.work@gmail.com>
2023-08-05 21:36:10 +0900

    本番サーバー用と開発サーバー用でfaviconファイルのパスを変更
    これで開発サーバーでもfaviconが見られる
    やり方は、viteのmodeを取得してEJSで条件分岐した

--- vite.config.js
+++ vite.config.js
@@ -60,36 +60,38 @@ for (let i = 0; i < files.length; i++) {
   }
 */
 
-export default defineConfig({
-  root: './src', //開発ディレクトリ設定
-  build: {
-    outDir: '../dist', //出力場所の指定
-    emptyOutDir: true, //出力先のディレクトリを毎回空にする
-    rollupOptions: { //ファイル出力設定
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1];
-          //Webフォントファイルの振り分け
-          if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
-            extType = 'fonts';
-          }
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'images';
-          }
-          //ビルド時のCSS名を明記してコントロールする
-          if(extType === 'css') {
-            return `assets/css/style.css`;
-          }
-          return `assets/${extType}/[name][extname]`;
+export default ({ mode }) => {
+  return defineConfig({
+    root: './src', //開発ディレクトリ設定
+    build: {
+      outDir: '../dist', //出力場所の指定
+      emptyOutDir: true, //出力先のディレクトリを毎回空にする
+      rollupOptions: { //ファイル出力設定
+        output: {
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.name.split('.')[1];
+            //Webフォントファイルの振り分け
+            if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+              extType = 'fonts';
+            }
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'images';
+            }
+            //ビルド時のCSS名を明記してコントロールする
+            if(extType === 'css') {
+              return `assets/css/style.css`;
+            }
+            return `assets/${extType}/[name][extname]`;
+          },
+          chunkFileNames: 'assets/js/[name].js',
+          entryFileNames: 'assets/js/[name].js',
         },
-        chunkFileNames: 'assets/js/[name].js',
-        entryFileNames: 'assets/js/[name].js',
+        input: inputFiles,
       },
-      input: inputFiles,
     },
-  },
-  plugins: [
-    liveReload(['components/**/*.ejs']),
-    ViteEjsPlugin(siteSetting),
-  ],
-});
+    plugins: [
+      liveReload(['components/**/*.ejs']),
+      ViteEjsPlugin({siteSetting, mode}),
+    ],
+  });
+}
