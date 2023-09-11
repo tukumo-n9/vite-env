65a5ab6541c2803d138082b22634777dcc2e21cd
tukumo-n9 <tukumon9.work@gmail.com>
2023-09-11 22:43:10 +0900

    plopでページ用のSCSS・JSを作成できるようにした

---
+++ plopfile.js
@@ -0,0 +1,56 @@
+export default function (plop) {
+  plop.setGenerator("page", {
+    description: "ページのテンプレートを作成します",
+    prompts: [
+      {
+        type: "input",
+        name: "directory",
+        message: "ページのディレクトリを入力してください",
+      },
+      {
+        type: "input",
+        name: "slug",
+        message: "ページのスラッグを入力してください（スキップできます）",
+      },
+      {
+        type: "input",
+        name: "pageClass",
+        message: "bodyタグにつけるClass名を入力してください（スキップできます）",
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "ページのタイトルを入力してください（スキップできます）",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "ページの説明を入力してください（スキップできます）",
+      },
+    ],
+    actions: function (data) {
+      const pagePath = `src/{{directory}}/`;
+      const scssPath = `src/scss/project/`;
+      const jsPath = `src/js/pages/`;
+
+      return [
+        {
+          type: "add",
+          path: pagePath + "index.ejs",
+          templateFile: ".plop/page.ejs.hbs",
+        },
+        {
+          type: "add",
+          path: scssPath + "_p-{{slug}}.scss",
+          templateFile: ".plop/style.scss.hbs",
+        },
+        {
+          type: "add",
+          path: jsPath + "{{slug}}.js",
+          templateFile: ".plop/script.js.hbs",
+        },
+      ];
+    },
+  });
+};
+
